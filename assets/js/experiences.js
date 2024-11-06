581614e7b3273a488dcef155865ba82963aafee1
Yohann Zapart <yzpt.de@gmail.com>
2024-11-06 10:23:27 +0100

    feeding content

---
+++ assets/js/experiences.js
@@ -0,0 +1,176 @@
+// content.js
+
+// Kooling.io (EN) septembre 2024 - aujourd'hui
+// Analyzing GPS tracking for a testing team within the company.
+// Evaluating and improving segmentation and transport mode detection algorithms
+// Deploying numerous data visualization dashboards and parameterized automated analyses (STDBSCAN, segmenters, tracking).
+
+
+
+// Développeur Python
+// Winslow MacDonald (US) | Novembre 2024
+// Développement d'un dashboard de visualisation d'articles historiques.
+
+// Timelines, cartographies et listing des articles de revues historiques.
+// Ajout/suppression/modification des données par l'utilisateur.
+// Création d'une animation de carte temporelle avec Plotly.
+// Data Analyst
+// Nextérité (FR) | Avril - Juin 2024
+// Traitement, analyse et visualisation de trackings GPS issues de données MPD (Mobile Positionning Data) pour définir les tendances de mobilité dans une grande ville française.
+
+// Déploiement de dashboards de visualisation de données et d'analyses automatisées.
+// Développement d'algorithmes de détection pour identifier le mode de transport utilisé par les utilisateurs suivis, en appliquant des règles déductives et des modèles de machine learning (ST-DBSCAN, K-Mean, features engineering, classification et clustering non supervisé).
+// Littérature scientifique : revues principalement de Paria Sadeghian et Yu Zheng.
+// Développeur Python / Data Analyst
+// KXI Wildertech (CAN) | Juin - Juillet 2024
+// Analyse automatisée de de séries temporelles issues d'une batterie de capteurs (format MDF4) implentés dans des véhicules de tests.
+
+// Traitement de signal automatisé (détection et caractérisation des virages, compressions de suspensions limites, positionnement GPS des détections, tangage, etc.)
+// Déploiement d'une application web permettant un reporting automatisé des données MDF4 en html, pdf et json pour l'export de features.
+// Tracking GPS avec localisations géographique des détections automatisées.
+// Software engineer
+// Solve42 GmBH (DE) | Juillet - Août 2024
+// Développement de fonctionnalités diverses au sein d'une application RAG/LLM.
+
+// Application des principes SOLID dans un esprit de clean-code.
+// Développement de test unitaires (pyTest).
+
+// Professeur certifié de mathématiques, Education Nationale, Académie de Lille
+// Enseignant de mathématiques et sciences physiques en lycée professionnel. Je m’étais spécialisé
+// dans la prise en charge d’élèves en rescolarisation et/ou avec de fortes difficultés scolaires et/ou
+// sociales (classes de CAP, 3PM, post-ULIS/SEGPA).
+// Mise en place d’une application web d’entraînement aux calculs. (ZapMaths)
+
+// 2015 - 2023
+
+// Ingénieur de recherche au CNRS, opto-électronique et imagerie TeraHertz
+// IEMN: Institut d’Electronique, de Micro-électronique et Nanotechnologies, Villeneuve-d’Ascq
+// Mise en place et programmation (LabView) de banc de mesures de dispositifs de détection de
+// rayonnement THz, Imagerie THz, développement d’algorithmes de traitement d’images (MATLAB).
+
+// Data analyst freelance, VIP Jet-Catering (FR)
+// Deployment of several classic dashboards (Dash/Plotly + Flask) focused on business.
+
+const experiences = [
+    {
+        title: "Développeur Python",
+        company: "Winslow MacDonald (US)",
+        date: "Novembre 2024",
+        description: "Développement d'un dashboard de visualisation d'articles historiques.",
+        tasks: [
+            "Timelines, cartographies et listing des articles de revues historiques.",
+            "Ajout/suppression/modification des données par l'utilisateur.",
+            "Création d'une animation de carte temporelle avec Plotly."
+        ]
+    },
+    {
+        title: "Data Analyst / Développeur Python + ESP32",
+        company: "Kooling.io (EN)",
+        date: "Septembre 2024 - aujourd'hui",
+        description: "Analyse de tracking GPS & développement d'algorithmes de détection automatique de mode de transport.",
+        tasks: [
+            "Évaluation et amélioration des algorithmes de segmentation et de détection des modes de transport.",
+            "Déploiement de plusieurs tableaux de bord de visualisation de données et analyses automatisées paramétrées (STDBSCAN, segmentation, suivi, évaluation d'algorithmes).",
+            "Développement d'une carte électronique de tracking GPS : ESP32 + Neo6M + sim800L."
+        ]
+    },
+    {
+        title: "Développeur Python / Data Analyst",
+        company: "KXI Wildertech (CAN)",
+        date: "Juin - Juillet 2024",
+        description: "Analyse automatisée de de séries temporelles issues d'une batterie de capteurs (format MDF4) implentés dans des véhicules de tests.",
+        tasks: [
+            "Traitement de signal automatisé (détection et caractérisation des virages, compressions de suspensions limites, positionnement GPS des détections, tangage, etc.)",
+            "Déploiement d'une application web permettant un reporting automatisé des données MDF4 en html, pdf et json pour l'export de features.",
+            "Tracking GPS avec localisations géographique des détections automatisées."
+        ]
+    },
+    {
+        title: "Software engineer",
+        company: "Solve42 GmBH (DE)",
+        date: "Juillet - Août 2024",
+        description: "Développement de fonctionnalités diverses au sein d'une application RAG/LLM.",
+        tasks: [
+            "Application des principes SOLID dans un esprit de clean-code.",
+            "Développement de test unitaires (pyTest)."
+        ]
+    },
+    {
+        title: "Data Analyst",
+        company: "Nextérité (FR)",
+        date: "Avril - Juin 2024",
+        description: "Traitement, analyse et visualisation de trackings GPS issues de données MPD (Mobile Positionning Data) pour définir les tendances de mobilité dans une grande ville française.",
+        tasks: [
+            "Déploiement de dashboards de visualisation de données et d'analyses automatisées.",
+            "Développement d'algorithmes de détection pour identifier le mode de transport utilisé par les utilisateurs suivis.",
+            "Littérature scientifique : revues principalement de <a href='allo'>Paria Sadeghian</a> et Yu Zheng."
+        ]
+    },
+    {
+        title: "Web scrapping",
+        company: "Stratalis Ltd (FR)",
+        date: "Avril 2024",
+        description: "Développement de scripts de scrapping avec Scrapy, Selenium, BeautifulSoup, etc.",
+        tasks: [
+            "Rotation des User-Agent et proxies",
+            "Interception AJAX",
+        ]
+    },
+    {
+        title: "Développeur Python",
+        company: "VIP Jet-Catering (FR)",
+        date: "avril 2024",
+        description: "Développement d'un dashboard orienté business.",
+        tasks: [],
+    },
+    {
+        title: "Professeur certifié de mathématiques",
+        company: "Education Nationale, Académie de Lille",
+        date: "2015 - 2023",
+        description: "Enseignant de mathématiques et sciences physiques en lycée professionnel. Je m’étais spécialisé dans la prise en charge d’élèves en rescolarisation et/ou avec de fortes difficultés scolaires et/ou sociales (classes de CAP, 3PM, post-ULIS/SEGPA).",
+        tasks: [
+            "Développement d’une application web d’entraînement aux calculs. (ZapMaths)",
+            "Apprentissage de la programmation en Python & Arduino",
+            "Création d'une chaîne YouTube de tutoriels de mathématiques",
+            "Veille didactique axée sur les neurosiences et l'exploitation du flow dans les apprentissages."
+        ]
+    },
+    {
+        title: "Ingénieur de recherche au CNRS, opto-électronique et imagerie TeraHertz",
+        company: "IEMN : Institut d’Electronique, de Micro-électronique et Nanotechnologies, Villeneuve-d’Ascq",
+        date: "2013 - 2015",
+        description: "Mise en place et programmation de bancs de mesures de dispositifs de détection de rayonnement THz orbitants autour d'un analyseur de réseau vectoriel (bande 0-500 Ghz).",
+        tasks: [
+            "Orchestration : LabView",
+            "Algorithmes de traitement d'images : MATLAB",
+        ]
+    }
+];
+
+// Function to render experiences dynamically
+const renderExperiences = () => {
+    const experienceList = document.getElementById("experience-list");
+
+    experiences.forEach(exp => {
+        const item = document.createElement("div");
+        item.classList.add("item", "mb-5");
+
+        item.innerHTML = `
+            <div class="item-heading row align-items-center mb-2">
+                <h4 class="item-title col-12 col-md-6 col-lg-6 mb-2 mb-md-0">${exp.title}</h4>
+                <div class="item-meta col-12 col-md-6 col-lg-6 text-muted text-start text-md-end">${exp.company} | ${exp.date}</div>
+            </div>
+            <div class="item-content">
+                <p class="mb-1">${exp.description}</p>
+                <ul class="resume-list">
+                    ${exp.tasks.map(task => `<li>${task}</li>`).join('')}
+                </ul>
+            </div>
+        `;
+
+        experienceList.appendChild(item);
+    });
+};
+
+// Call the function to render experiences when the page loads
+window.onload = renderExperiences;
