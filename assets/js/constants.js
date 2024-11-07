6a2b3d7a7279c584992553b3f8ffbac565cd19b7
Yohann Zapart <yzpt.de@gmail.com>
2024-11-07 18:11:35 +0100

    starting projects

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,6 +1,69 @@
 // experiences.js
+
+const projects = [
+    {
+        title: "V'Lille GCP ETL",
+        description: "ETL de l'état des stations V'Lille sur GCP",
+        stack: ["GCP : BigQuery, Storage, Cloud Functions, Dataproc, Pub/Sub, Run", "Flask", "Docker", "Javascript", "Google Maps API"],
+        github: "https://github.com/yzpt/vlille_gcp",
+        demo: "https://vlille-v1-app-service-dxpal7wuuq-od.a.run.app/",
+        images: [
+            "assets/images/diagram_vlille.svg",
+            "assets/images/flask_dashboard.png",
+        ],
+    },
+    {
+        title: "Geolife GPS Tracking",
+        description: "Visualisation de trackings GPS du dataset Geolife, démonstration de la clusterisation ST-DBSCAN",
+        stack: ["Dash", "Plotly", "GeoPandas", "Scikit-Learn"],
+        images: [
+            "assets/images/geolife_dash1.png",
+            "assets/images/geolife_dash2.png",
+        ],
+        
+    }
+]
+
+
+const renderProjects = () => {
+    const projectList = document.getElementById("projects-list");
+
+    projects.forEach(project => {
+        const item = document.createElement("div");
+        item.classList.add("item", "mb-3");
+
+        item.innerHTML = `
+            <div class="item-heading row align-items-center mb-2">
+                <h4 class="item-title col-12 col-md-6 col-lg-6 mb-2 mb-md-0">${project.title}</h4>
+            </div>
+            <div class="item-content">
+                ${project.stack ? `
+                    <p class="m-0" style="color: #54B689; font-size: .8rem;">${project.stack.join(' | ')}</p>
+                ` : ''}
+                <p class="mb-1">${project.description}</p>
+                <div class="d-flex justify-content-between"></div>
+                <div class="row mt-3">
+                    ${project.images.map(img => `
+                        <div class="col-12 col-md-6 mb-3">
+                            <img src="${img}" class="img-fluid" alt="project image">
+                        </div>
+                    `).join('')}
+                </div>
+            </div>
+        `;
+        projectList.appendChild(item);
+    });
+};
+
+
+
+
+
+
+
+
 const introduction = [
-    "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.",
+    "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat",
 ]
 
 
@@ -18,7 +81,7 @@ const renderIntroduction = () => {
             introductionList.appendChild(item);
         });
     } else {
-        console.error("Element with id 'introduction-list' not found.");
+        console.error("Element with id 'introduction-list' not found");
     }
 };
 
@@ -29,9 +92,9 @@ const experiences = [
         company: "Kooling.io (EN)",
         date: "Septembre 2024 - aujourd'hui",
         tasks: [
-            "Analyse de tracking GPS & développement d'algorithmes de détection automatique de mode de transport.",
-            "Déploiement de plusieurs tableaux de bord de visualisation de données et analyses automatisées paramétrées (STDBSCAN, segmentation, tracking features, évaluation d'algorithmes).",
-            "Développement d'une carte électronique de tracking GPS : ESP32 + Neo6M + sim800L."
+            "Analyse de tracking GPS & développement d'algorithmes de détection automatique de mode de transport",
+            "Déploiement de plusieurs tableaux de bord de visualisation de données et analyses automatisées paramétrées (STDBSCAN, segmentation, tracking features, évaluation d'algorithmes)",
+            "Développement d'une carte électronique de tracking GPS : ESP32 + Neo6M + sim800L"
         ],
         description: "",
         stack: ["Pandas", "Dash", "Plotly", "Postgresql", "C++", "MicroPython", "Scikit-Learn"],
@@ -42,7 +105,7 @@ const experiences = [
         date: "Novembre 2024",
         tasks: [
             "Développement d'un dashboard de consultation d'articles historiques",
-            "Timelines par auteurs, cartographie des articles, édition des données en ligne."
+            "Timelines par auteurs, cartographie des articles, édition des données en ligne"
         ],
         stack: ["Pandas", "Dash", "Plotly", "Docker", "GCP Cloud Run"],
     },
@@ -51,9 +114,9 @@ const experiences = [
         company: "KXI Wildertech (CAN)",
         date: "Juin - Juillet 2024",
         tasks: [
-            "Analyse automatisée de de séries temporelles issues d'une batterie de capteurs (format MDF4) implentés dans des véhicules de tests.",
+            "Analyse automatisée de de séries temporelles issues d'une batterie de capteurs (format MDF4) implentés dans des véhicules de tests",
             "Traitement de signal automatisé (détection et caractérisation des virages, compressions de suspensions limites, positionnement GPS des détections, tangage, etc.)",
-            "Déploiement d'une application web permettant un reporting automatisé.",
+            "Déploiement d'une application web permettant un reporting automatisé",
         ],
         description: "",
         stack: ["Pandas", "Flask", "Plotly", "Leaflet", "Jupyter", "Matplotlib", "GCP", "BigQuery"],
@@ -63,9 +126,9 @@ const experiences = [
         company: "Solve42 GmBH (DE)",
         date: "Juillet - Août 2024",
         tasks: [
-            "Développement de fonctionnalités diverses d'une application RAG/LLM avec FastAPI.",
+            "Développement de fonctionnalités diverses d'une application RAG/LLM avec FastAPI",
             "Application des principes SOLID",
-            "Développement des tests unitaires avec pyTest."
+            "Développement des tests unitaires avec pyTest"
         ],
         description: "",
         stack: ["FastAPI", "pytest", "Docker", "PostgreSQL", "Chroma"],
@@ -75,10 +138,10 @@ const experiences = [
         company: "Nextérité (FR)",
         date: "Avril - Juin 2024",
         tasks: [
-            "Traitement, analyse et visualisation de trackings GPS issues de données MPD (Mobile Positionning Data) pour définir les tendances de mobilité dans une grande ville française.",
-            "Déploiement de dashboards de visualisation de données et d'analyses automatisées.",
-            "Développement d'algorithmes de détection de mode de transport.",
-            "Littérature scientifique : <a href='allo'>Paria Sadeghian</a> et Yu Zheng."
+            "Traitement, analyse et visualisation de trackings GPS issues de données MPD (Mobile Positionning Data) pour définir les tendances de mobilité dans une grande ville française",
+            "Déploiement de dashboards de visualisation de données et d'analyses automatisées",
+            "Développement d'algorithmes de détection de mode de transport",
+            "Littérature scientifique : <a href='allo'>Paria Sadeghian</a> et Yu Zheng"
         ],
         description: "",
         stack: ["GeoPandas", "Leaflet", "Javascript", "OpenStreetMap", "GeoPy", "Scikit-Learn", "Flask", "Docker", "Dash", "Plotly", "GCP Cloud Run", "GCP BigQuery"],
@@ -88,7 +151,7 @@ const experiences = [
         company: "Stratalis Ltd (FR)",
         date: "Avril 2024",
         tasks: [
-            "Rotation de proxys, interceptions AJAX.",
+            "Rotation de proxys, interceptions AJAX",
         ],
         description: "",
         stack: ["Scrapy", "BeautifulSoup", "Selenium", "TypeScript"],
@@ -98,7 +161,7 @@ const experiences = [
         company: "VIP Jet Catering (FR)",
         date: "Avril 2024",
         tasks: [
-            "Dashboard d'analyse des ventes",        
+            "Dashboard d'analyse des ventes",
         ],
         stack: ["Pandas", "Dash", "Plotly", "Docker", "GCP Cloud Run"],
     },
@@ -108,8 +171,8 @@ const experiences = [
         date: "2015 - 2023",
         // description: "",
         tasks: [
-            "Enseignant de mathématiques et sciences physiques en lycée professionnel.",
-            "Spécialisé élèves en rescolarisation et/ou avec scolarité adaptée (classes de CAP, 3PM, post-ULIS/SEGPA, Allophones).",
+            "Enseignant de mathématiques et sciences physiques en lycée professionnel",
+            "Elèves en scolarité adaptée (classes de CAP, 3PM, post-ULIS/SEGPA, Allophones)",
             "Développement d’une application web d’entraînement aux calculs. (ZapMaths)",
             "Apprentissage de la programmation en Python & Arduino",
             "Création d'une chaîne YouTube de tutoriels de mathématiques",
@@ -170,7 +233,8 @@ const diplomes = [
         title: "Diplôme d'ingénieur en instrumentation scientifique",
         school: "Polytech'Lille",
         date: "2013",
-        description: "Optique, électronique, photonique, traitement du signal, mesure, programmation, analyse de données, statistiques",
+        description: undefined,
+        stack: ["Optique", "Electronique", "Photonique", "Traitement du signal", "Mesure", "Programmation", "Analyse de données", "Statistiques"],
         tasks: [],
         hr: true,
     },
@@ -178,7 +242,8 @@ const diplomes = [
         title: "Master 2 recherche en micro-électronique et nanotechnologies",
         school: "Université de Lille",
         date: "2013",
-        description: "Opto-électronique Terahertz, semi-conducteurs, hyperfréquences, nanotechnologies, traitement du signal, programmation",
+        description: undefined,
+        stack: ["Littérature & bibliographie scientifique", "Opto-électronique", "Semi-conducteurs", "Hyperfréquences", "Nanotechnologies"],
         tasks: [],
         hr: true,
     },
@@ -215,6 +280,9 @@ const renderDiplomes = () => {
             <div class="item-meta col-12 col-md-3 col-lg-3 text-muted text-start text-md-end">${dip.school} | ${dip.date}</div>
             </div>
             <div class="item-content">
+            ${dip.stack ? `
+                <p class="m-0" style="color: #54B689; font-size: .8rem;">${dip.stack.join(' | ')}</p>
+            ` : ''}
             ${dip.description ? `
                 <p class="mb-1">${dip.description}</p>
             ` : ''}
@@ -244,7 +312,7 @@ const formations = [
         title: "DU/M1 Master MEEF Mathématiques",
         school: "IUFM/ESPE/INSPE de Villeneuve d'Ascq",
         date: "2016 - 2018",
-    }, 
+    },
 ]
 
 
@@ -282,5 +350,6 @@ window.onload = () => {
     renderExperiences();
     renderDiplomes();
     renderIntroduction();
-    renderFormations();
+    // renderFormations();
+    renderProjects();
 };
